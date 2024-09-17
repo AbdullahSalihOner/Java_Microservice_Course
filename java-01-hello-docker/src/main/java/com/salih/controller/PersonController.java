2fba5a4deef005a13988390b1c31dc90b89066dd
Abdullah Salih Öner <riyadlioner00@gmail.com>
2024-09-17 16:51:57 +0300

    Dockerfile Added
    Dcokerfile and Dcoker prompt entegrated to project

--- java-01-hello-docker/src/main/java/com/salih/controller/PersonController.java
+++ java-01-hello-docker/src/main/java/com/salih/controller/PersonController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping("/api/person")
 public class PersonController {
@@ -11,7 +13,7 @@ public class PersonController {
     // http://localhost:8080/api/person/hello
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from PersonController";
+        return "Aloo Hello from Project Version 2 : " + LocalDateTime.now();
     }
 
 }

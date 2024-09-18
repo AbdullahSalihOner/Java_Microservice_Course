ab82279f21af3813ee1e42dc2bffb24714872df6
Abdullah Salih Öner <riyadlioner00@gmail.com>
2024-09-19 01:14:48 +0300

    day2 project started

---
+++ java-03-microservice-project/day2/java-03-microservice-project/AuthService/src/main/java/com/salih/repository/IAuthRepository.java
@@ -0,0 +1,18 @@
+package com.salih.repository;
+
+import com.salih.model.Auth;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Repository
+public interface IAuthRepository extends JpaRepository <Auth, Long> {
+
+    Boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    Optional <Auth> findByUsernameAndPassword(String username, String password);
+}
---
+++ java-03-microservice-project/day2/java-03-microservice-project/src/main/java/com/salih/Java03MicroserviceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.salih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java03MicroserviceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java03MicroserviceProjectApplication.class, args);
+    }
+
+}

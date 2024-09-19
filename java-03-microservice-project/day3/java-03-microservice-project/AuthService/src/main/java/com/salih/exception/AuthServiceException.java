02773eff1e7ebb7cf75932b273050946939d6a34
Abdullah Salih Öner <64440843+AbdullahSalihOner@users.noreply.github.com>
2024-09-19 23:26:42 +0300

    Day3 started

---
+++ java-03-microservice-project/day3/java-03-microservice-project/AuthService/src/main/java/com/salih/exception/AuthServiceException.java
@@ -0,0 +1,22 @@
+package com.salih.exception;
+
+
+import lombok.Getter;
+
+@Getter
+public class AuthServiceException extends RuntimeException {
+
+    private final ErrorType type;
+
+
+    public AuthServiceException(ErrorType type) {
+        this.type = type;
+    }
+
+    /*
+    public AuthServiceException(ErrorType type, String message) {
+        this.type = type;
+    }
+    */
+
+}
---
+++ java-03-microservice-project/day3/java-03-microservice-project/AuthService/src/main/java/com/salih/repository/IAuthRepository.java
@@ -0,0 +1,17 @@
+package com.salih.repository;
+
+import com.salih.model.Auth;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IAuthRepository extends JpaRepository <Auth, Long> {
+
+    Boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    Optional <Auth> findByUsernameAndPassword(String username, String password);
+}
